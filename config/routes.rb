bd0e8061bc6252dfa0472442aaecdab325aec355
Haruna Fujita <fujitaharuna.p@gmail.com>
2025-11-14 10:09:23 +0900

    pushing app to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
+  root to: 'lists#index'
+
   resources :lists, only: [:index, :show, :new, :create] do
     resources :bookmarks, only: [:new, :create, :destroy]
   end
