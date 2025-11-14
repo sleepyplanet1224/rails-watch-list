79ae408660842c742490b9ba1b850c7a24382d6e
Haruna Fujita <fujitaharuna.p@gmail.com>
2025-11-14 15:53:52 +0900

    Fixed delete button

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   root to: 'lists#index'
 
   resources :lists, only: [:index, :show, :new, :create] do
-    resources :bookmarks, only: [:new, :create, :destroy]
+    resources :bookmarks, only: [:new, :create]
   end
+
+  resources :bookmarks, only: :destroy
 end
